498f7740396884e7b8ad248940c6bce3e8e22c5e
Zachary Knudson <zac520@aol.com>
2014-08-10 19:20:43 -0400

    Added Rube Loader

--- core/src/com/NZGames/Box2DWorld/MainGame.java
+++ core/src/com/NZGames/Box2DWorld/MainGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.gushikustudios.rube.loader.RubeSceneLoader;
 
 public class MainGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -13,7 +14,10 @@ public class MainGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
+
+        //test Rube Loader
+        RubeSceneLoader loader = new RubeSceneLoader();
 	}
 
 	@Override
@@ -21,7 +25,7 @@ public class MainGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
 		batch.end();
 	}
 }
