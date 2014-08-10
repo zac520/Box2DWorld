14d308e01358e96f7dfe5755a9cccb76895290b6
Zachary Knudson <zac520@aol.com>
2014-08-10 19:04:55 -0400

    Blank Libgdx Project

---
+++ desktop/src/com/NZGames/Box2DWorld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.NZGames.Box2DWorld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.NZGames.Box2DWorld.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
